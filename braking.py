30e9e8e1e481c7a7e778211e82baf4ae8a63f869
tsiri1612 <1612tsiri@gmail.com>
2026-07-20 15:27:17 +0530

    Add reaction distance and total stopping distance calculations

--- braking.py
+++ braking.py
@@ -30,6 +30,34 @@ else:
 
 GRAVITY=9.81
 
-d = (speed ** 2)/(2 * mu * GRAVITY)
+braking_distance = (speed ** 2)/(2 * mu * GRAVITY)
 
-print(f"\nEstimated braking distance: {d:.2f} m\n")
+print("\nDriver conditions")
+print("1.Alert")
+print("2.Average")
+print("3.Fatigued")
+print("4.Distracted")
+
+driver=int(input("\nSelect driver condition: "))
+
+rxn_time=0
+
+if driver == 1:
+    rxn_time = 0.8
+elif driver == 2:
+    rxn_time = 1.5
+elif driver == 3:
+    rxn_time = 2.0
+elif driver == 4:
+    rxn_time = 2.5
+else:
+    print("Invalid driver condition")
+    exit()
+
+reaction_distance = speed * rxn_time
+
+stopping_distance = braking_distance + reaction_distance
+
+print(f"\n{'Estimated reaction distance':<30}: {reaction_distance:.2f} m")
+print(f"{'Estimated braking distance':<30}: {braking_distance:.2f} m")
+print(f"{'Total stopping distance':<30}: {stopping_distance:.2f} m\n")
